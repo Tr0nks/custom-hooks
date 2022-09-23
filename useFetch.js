d0754bee0eea3ec55c40cafa8193277085b94468
AngeL Troncoso <tronkoso1983@gmail.com>
2022-09-23 16:54:00 -0500

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+
+    const [state, setstate] = useState({
+        data: null,
+        isLoading: true,
+        hasError: null
+    });
+
+    const getFetch = async() => {
+
+        setstate({
+            ...state,
+            isLoading : true,
+        });
+
+        const resp = await fetch(url);
+        const data = await resp.json(resp);
+
+        setstate({
+            data,
+            isLoading : false,
+            hasError : null
+        });
+        
+    }
+ 
+    
+
+    useEffect((url) => {
+        getFetch();
+        
+    }, [url]);
+ 
+
+    return {
+        
+        data: state.data,
+        isLoading : state.isLoading,
+        hasError : state.hasError
+
+    };
+}
